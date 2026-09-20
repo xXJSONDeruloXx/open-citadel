a3149f024e852e3155da7cb0179e651c3aee7bfa
Kurt Himebauch <136133082+xXJSONDeruloXx@users.noreply.github.com>
2026-09-20 14:26:42 -0400

    pages: publish Citadel web preview

--- android/emulator_control.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/*
- */
-void emulator_control_init(void);
-bool emulator_control_tick(long frame);
-void emulator_control_after_draw(long frame, int width, int height);
-void emulator_control_shutdown(long frame);
