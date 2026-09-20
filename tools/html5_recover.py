77a76f6e2211a57889b6003640682dc3fdb7de0f
Kurt Himebauch <136133082+xXJSONDeruloXx@users.noreply.github.com>
2026-09-20 03:00:49 -0400

    tools: recover Citadel data and memory payloads

--- tools/html5_recover.py
+++ tools/html5_recover.py
@@ -17,7 +17,7 @@ from pathlib import Path
 ROOT = "http://www.unrealengine.com/html5/"
 STAMP = "20130504031131"
 UA = "Open-Citadel-HTML5-Recovery/1.0"
-CORE = ("UDKGame_Data.js", "UDKGame-Browser-Shipping.js")
+CORE = (\n    "UDKGame_Data.js",\n    "UDKGame_Data.data",\n    "UDKGame-Browser-Shipping.js",\n    "UDKGame-Browser-Shipping.js.mem",\n)
 
 
 def archived(original: str) -> str:
